08498aae8d10eb345b3eb5b457b3ece5dd311717
aubaguevictor <153274832+Victoraubague@users.noreply.github.com>
2024-09-06 09:01:39 +0200

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,27 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: CountriesListScreen(),
     );
   }
 }
 
 class CountriesListScreen extends StatefulWidget {
+  const CountriesListScreen({super.key});
+
   @override
   _CountriesListScreenState createState() => _CountriesListScreenState();
 }
 
-class _CountriesListScreenState extends State<CountriesListScreen> {
+class _CountriesListScreenState extends State<CountriesListScreen> with SingleTickerProviderStateMixin {
   List<dynamic> countries = [];
+  bool isSorted = false;
+  late AnimationController _controller;
 
   Future<void> fetchCountries() async {
     final response = await http.get(Uri.parse('https://restcountries.com/v3.1/all'));
@@ -35,32 +41,70 @@ class _CountriesListScreenState extends State<CountriesListScreen> {
     }
   }
 
+  //tout ce code avant sert à cherccher les données
+
+
   @override
   void initState() {
     super.initState();
     fetchCountries();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void sortCountries() {
+    setState(() {
+      if (!isSorted) {
+        countries.sort((a, b) => a['name']['common'].compareTo(b['name']['common']));
+      } else {
+        countries.shuffle(); 
+      }
+      isSorted = !isSorted;
+    });
+
+
+    _controller.forward(from: 0.0);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Tout les pays'),
+        title: const Text('Countries of the World'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.sort_by_alpha),
+            onPressed: sortCountries,
+          ),
+        ],
       ),
+
+    
       body: countries.isEmpty
-          ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: countries.length,
-              itemBuilder: (context, index) {
+          ? const Center(child: CircularProgressIndicator())
+          : AnimatedList(
+              initialItemCount: countries.length,
+              itemBuilder: (context, index, animation) {
                 final country = countries[index];
-                return ListTile(
-                  leading: Image.network(
-                    country['flags']['png'],
-                    width: 50,
-                    height: 50,
+                return SizeTransition(
+                  sizeFactor: animation,
+                  child: ListTile(
+                    leading: Image.network(
+                      country['flags']['png'],
+                      width: 50,
+                      height: 50,
+                    ),
+                    title: Text(country['name']['common']),
+                    subtitle: Text('Capital: ${country['capital'] != null ? country['capital'][0] : 'N/A'}'),
                   ),
-                  title: Text(country['name']['common']),
-                  subtitle: Text('Capital: ${country['capital'] != null ? country['capital'][0] : 'N/A'}'),
                 );
               },
             ),
